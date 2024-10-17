535e967e28e43cfe8f090c375eb7a8fe995f7cee
Adarsh Gupta <adarshguptamalawar@gmail.com>
2024-10-17 11:41:48 +0530

    frontend changes

--- src/apiservice/axios.js
+++ src/apiservice/axios.js
@@ -1,18 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export default axios.create({
     baseURL: BASE_URL,
-}) 
-
-export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    withCredentials: true
-})
+    withCredentials: true // Ensure credentials are included
+});
 
 
 
